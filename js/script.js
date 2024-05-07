cfb0bab82cd39ce5215b3e0f2e9652737254c3bf
lionwin <kjyy2028@gmail.com>
2024-05-08 04:20:48 +0900

    아스토프르랙스 중간완성

--- js/script.js
+++ js/script.js
@@ -1,17 +1,90 @@
 $(function () {
   var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    slidesPerGroup: 3,
+    slidesPerView: 2,
+    spaceBetween: 26.99,
+    slidesPerGroup: 2,
     loop: true,
     loopFillGroupWithBlank: true,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
     },
+    // autoplay: {
+    //   delay: 3000,
+    //   disableOnInteraction: false,
+    // },
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
   });
+  var swiper = new Swiper(".mySwiper2", {
+    slidesPerView: 3,
+    slidesPerGroup: 1,
+
+    spaceBetween: 50,
+    centeredSlides: true,
+    // preventClicks: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    loop: true,
+    centeredSlides: true,
+    on: {
+      init: function () {
+        $(".swiper-slide").addClass("changed");
+      },
+      slideChangeTransitionStart: function () {
+        $(".swiper-slide").addClass("changing");
+        $(".swiper-slide").removeClass("changed");
+      },
+      slideChangeTransitionEnd: function () {
+        $(".swiper-slide").removeClass("changing");
+        $(".swiper-slide").addClass("changed");
+      },
+      slideChange: function () {
+        const sIdx = this.realIndex;
+        // console.log(this);
+
+        const slideInfo = $(".slide-info li");
+        slideInfo.removeClass("active");
+        slideInfo.eq(sIdx).addClass("active");
+      },
+    },
+  });
+  new Vivus(
+    "responsible",
+    {
+      duration: 100,
+      type: "delayed",
+      animTimingFunction: Vivus.EASE,
+    },
+    function (obj) {
+      obj.el.classList.add("finished");
+    }
+  );
+  new Vivus(
+    "Transparent",
+    {
+      duration: 100,
+      type: "delayed",
+      animTimingFunction: Vivus.EASE,
+    },
+    function (obj) {
+      obj.el.classList.add("finished");
+    }
+  );
+  new Vivus(
+    "Sagacious",
+    {
+      duration: 100,
+      type: "delayed",
+      animTimingFunction: Vivus.EASE,
+    },
+    function (obj) {
+      obj.el.classList.add("finished");
+    }
+  );
+  AOS.init();
 });
